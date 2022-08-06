b27128d91ea59f840926d598cb1141705f005156
YanaReznikova <108279798+YanaReznikova@users.noreply.github.com>
2022-08-06 23:04:14 +0400

    Add files via upload

---
+++ Task_13/Program.cs
@@ -0,0 +1,13 @@
+﻿/*
+Напишите программу, которая выводит третью цифру заданного 
+числа или сообщает, что третьей цифры нет.
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6
+*/
+Console.Clear();
+
+Console.WriteLine("Введите любое число: ");
+string third = Console.ReadLine();
+int thirdNum = int.Parse(third);
+Console.WriteLine(thirdNum>99 ? thirdNum.ToString()[2] : "Третьего числа нет");
---
+++ Task_15/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+6 -> да
+7 -> да
+1 -> нет
+*/
+Console.Clear();
+Console.WriteLine("Введите любое число: ");
+string anyNum = Console.ReadLine();
+int dayNum = int.Parse(anyNum);
+
+if(dayNum == 6 || dayNum == 7) {
+    Console.WriteLine("Сегодня выходной день!");
+}
+else if(dayNum > 7 || dayNum < 1) {
+    Console.WriteLine("Такого дня недели не существует!");
+}
+else {
+    Console.WriteLine("Сегодня будний день!");
+}
